f5ce6e001cdb7d2d077c8680af6d3ab4690d5327
ClairvoyantHG <wjdgory@gmail.com>
2026-09-14 18:17:12 +0900

    [feat] 닉네임 변경 스크립트 구현

---
+++ Assets/02.Scripts/Account/ChangePlayerNameService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+using MySqlConnector;
+using Cysharp.Threading.Tasks;
+
+public class ChangePlayerNameService
+{
+    private ChangePlayerNameViewModel _viewModel;
+
+    public ChangePlayerNameService()
+    {
+        _viewModel = new ChangePlayerNameViewModel();
+        _viewModel.SetService(this);
+    }
+
+    public ChangePlayerNameViewModel GetViewModel()
+    {
+        return _viewModel;
+    }
+
+    public async UniTask<bool> TryChangePlayerNameAsync(long uid, string newName)
+    {
+        bool isSuccess = false;
+
+        if (uid == 0 || newName == "") return isSuccess;
+
+        using (MySqlConnection conn = new MySqlConnection(DBConfig.ConnectionString))
+        {
+            try
+            {
+                await conn.OpenAsync();
+
+                string query = $"UPDATE {DBConfig.UserGameTable} SET User_Name = @newName WHERE User_UID = @uid;";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@newName", newName);
+                    cmd.Parameters.AddWithValue("@uid", uid);
+
+                    int rowsAffected = await cmd.ExecuteNonQueryAsync();
+
+                    if (rowsAffected > 0)
+                    {
+                        isSuccess = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError(ex.Message);
+            }
+        }
+
+        return isSuccess;
+    }
+}
---
+++ Assets/02.Scripts/Account/ChangePlayerNameView.cs
@@ -0,0 +1,97 @@
+﻿using System.ComponentModel;
+using UnityEngine;
+using TMPro;
+
+public class ChangePlayerNameView : UIBase
+{
+    [SerializeField] private TMP_InputField InputField_Name;
+    [SerializeField] private UIButton Button_Confirm;
+    [SerializeField] private UIButton Button_Close;
+
+    [SerializeField] private UIButton Button_BackgroundBlocker;
+
+    private ChangePlayerNameViewModel _vm;
+
+    private void Awake()
+    {
+        ChangePlayerNameService service = ServiceManager.Instance.ChangePlayerNameService;
+
+        if (service != null)
+        {
+            BindViewModel(service.GetViewModel());
+        }
+    }
+
+    public void BindViewModel(ChangePlayerNameViewModel vm)
+    {
+        _vm = vm;
+
+        _vm.PropertyChanged += OnPropChanged_View;
+        _vm.OnCompleteChangeName += OnCompleteChangeName_View;
+        _vm.OnFailChangeName += OnFailChangeName_View;
+    }
+
+    private void OnEnable()
+    {
+        Button_Confirm.BindOnClickButtonEvent(OnClickConfirm);
+        Button_Close.BindOnClickButtonEvent(OnClickClose);
+
+        if (Button_BackgroundBlocker != null)
+        {
+            Button_BackgroundBlocker.BindOnClickButtonEvent(OnClickClose);
+        }
+
+        InputField_Name.onValueChanged.AddListener(OnChangeName);
+    }
+
+    private void OnDisable()
+    {
+        InputField_Name.onValueChanged.RemoveListener(OnChangeName);
+    }
+
+    private void OnDestroy()
+    {
+        if (_vm != null)
+        {
+            _vm.PropertyChanged -= OnPropChanged_View;
+            _vm.OnCompleteChangeName -= OnCompleteChangeName_View;
+            _vm.OnFailChangeName -= OnFailChangeName_View;
+        }
+    }
+
+    private void OnPropChanged_View(object sender, PropertyChangedEventArgs e)
+    {
+    }
+
+    private void OnChangeName(string text)
+    {
+        if (_vm != null)
+        {
+            _vm.InputName = text;
+        }
+    }
+
+    private void OnClickConfirm()
+    {
+        if (_vm != null)
+        {
+            _vm.RequestChangePlayerName();
+        }
+    }
+
+    private void OnClickClose()
+    {
+        UIManager.Instance.CloseUI(UIRootType.PopupUI, UIType.ChangePlayerNameUI);
+    }
+
+    private void OnCompleteChangeName_View()
+    {
+        Debug.Log("인게임 닉네임 변경 성공!");
+        UIManager.Instance.CloseUI(UIRootType.PopupUI, UIType.ChangePlayerNameUI);
+    }
+
+    private void OnFailChangeName_View()
+    {
+        Debug.Log("인게임 닉네임 변경 실패.");
+    }
+}
