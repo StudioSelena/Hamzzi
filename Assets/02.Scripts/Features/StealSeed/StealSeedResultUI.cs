21a73faccd955a83aca46ea976e75c13de12fd3d
이나라 <dlskfk72@gmail.com>
2026-09-15 16:31:07 +0900

    [feat] 훔친 씨앗 보유 씨앗에 추가

--- Assets/02.Scripts/Features/StealSeed/StealSeedResultUI.cs
+++ Assets/02.Scripts/Features/StealSeed/StealSeedResultUI.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using Cysharp.Threading.Tasks;
+using TMPro;
 using UnityEngine;
 
 public class StealSeedResultUI : ViewBase
@@ -9,6 +10,8 @@ public class StealSeedResultUI : ViewBase
     [Header("씨앗 개수")]
     [SerializeField] private TMP_Text Text_StealSeedCount;
 
+    private int _stealSeedCount;
+
     private void OnEnable()
     {
         Button_Confirm.BindOnClickButtonEvent(OnClick_Confirm);
@@ -16,12 +19,26 @@ public class StealSeedResultUI : ViewBase
 
     private void OnClick_Confirm()
     {
+        AddSeedCount();
         UIManager.Instance.CloseStealSeedResultUI();
         UIManager.Instance.CloseStealSeedUI();
     }
 
+    private void AddSeedCount()
+    {
+        var userVm = ServiceManager.Instance.UserService.GetUserViewModel();
+        if(userVm != null)
+        {
+            userVm.AddSeedWithoutBuff(_stealSeedCount);
+
+            var userUid = ServiceManager.Instance.LoginService.GetViewModel().UserUID;
+            ServiceManager.Instance.UserService.SaveUserAsync(userUid).Forget();
+        }
+    }
+
     public void SetStealSeedCount(int stealSeedCount)
     {
+        _stealSeedCount = stealSeedCount;
         Text_StealSeedCount.text = stealSeedCount.ToString();
     }
 }
